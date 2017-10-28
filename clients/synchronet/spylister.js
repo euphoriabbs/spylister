0753390fe7bf48b07b47e5d82b4a3934f5458fdd
Dan Stephenson <ispyhumanfly@gmail.com>
2017-10-28 14:05:16 -0400

    A simple prototype for synchronet.

---
+++ clients/synchronet/spylister.js
@@ -0,0 +1,26 @@
+"use strict"
+
+load("http.js");
+load("modopts.js");
+
+var SpyLister = function() {
+
+    var server = "http://spylister.euphoriabbs.com/api/v0";
+
+    var getJSON = function(path) {
+        try {
+            return JSON.parse(
+                new HTTPRequest().Get(server + (typeof path == "undefined" ? "" : path))
+            );
+        } catch (err) {
+            log("SpyLister._getJSON: " + err);
+            return null;
+        }
+    };
+
+    this.filelist = function(path) {
+        return getJSON("/filelist")
+    };
+};
+
+log(SpyLister.filelist);

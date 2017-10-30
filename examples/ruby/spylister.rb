223c4161abbeb8b83074edd219d812431137e8ff
Dan Stephenson <ispyhumanfly@gmail.com>
2017-10-30 00:01:38 -0400

    Playing with prompts.

--- examples/ruby/spylister.rb
+++ examples/ruby/spylister.rb
@@ -8,6 +8,7 @@ require "json"
 require "yaml"
 
 require "timers"
+require "io/console"
 
 # SpyLister Color Scheme
 
@@ -18,6 +19,7 @@ theme = HighLine::ColorScheme.new do |style|
     style[:footer]             = [ :bold, :green, :on_black ]
     style[:even_row]           = [ :blue ]
     style[:odd_row]            = [ :grey ]
+    style[:winsize]             = [ :bold, :red, :on_grey ]
 end
 
 HighLine.color_scheme = theme
@@ -54,7 +56,7 @@ printf "\r                                           "
 nodes = RestClient.get "http://#{ARGV[0]}/api/v0/getnodes"
 nodes = JSON.parse nodes.body
 
-puts "\r"
+puts ""
 
 nodes.each do |node|
     node = RestClient.get "http://#{ARGV[0]}/api/v0/getnode/#{node}"
@@ -66,13 +68,13 @@ nodes.each do |node|
 
     switch = false
 
-    files.each do |filename|
+    files.each do |file|
 
         if switch == true
-            say "<%= color('#{filename.to_json}', :even_row ) %>"
+            say "<%= color('#{file.to_json}', :even_row ) %>"
             switch = false
         else
-            say "<%= color('#{filename.to_json}', :odd_row ) %>"
+            say "<%= color('#{file.to_json}', :odd_row ) %>"
             switch = true
         end
     end
@@ -84,8 +86,12 @@ puts ""
 
 timers = Timers::Group.new
 
-timers.now_and_every(5) {
-    ask("\r<%= color('Ready to quit?', :footer) %> ") { |q| q.default = "quit" }
+timers.now_and_every(1) {
+    rows, columns = $stdin.winsize
+    time = DateTime.now()
+
+    printf "\r: :: #{time} - #{columns}x#{rows}"
+    #ask("\r<%= color('Ready to quit?', :footer) %> ") { |q| q.default = "quit" }
 }
 
 loop { timers.wait }
